848c74070998d25ee41f20c78705cc60027a7cb0
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-11-16 17:30:30 +0530

    Time: 7 ms (37.71%), Space: 11.7 MB (67.18%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -9,16 +9,15 @@ public:
         vector<bool> included(21, 0);
         for(int i = index; i < nums.size(); i++) {
             if(!included[nums[i]+10]){
+                included[nums[i]+10] = true;
                 swap(nums[index], nums[i]);
                 findPermutations(ans, nums, index+1);
                 swap(nums[index], nums[i]);
-                included[nums[i]+10] = true;
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<bool> included(21, 0);
         findPermutations(ans, nums, 0);
         return ans;
     }
