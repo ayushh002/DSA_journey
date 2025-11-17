04e6c305b7d1f9c0bb63015d62fb996905b54681
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-11-18 00:44:48 +0530

    Time: 0 ms (100%), Space: 13.9 MB (6.19%) - LeetHub

---
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int solve(int n, vector<int>& nums){
+        if(n==1) return nums[0];
+        vector<int> newNums;
+        for(int i = 0; i<n; i++){
+            if(i<n/2 && i%2==0){
+                newNums.push_back(min(nums[2 * i], nums[2 * i + 1]));
+            }
+            else if(i<n/2){
+                newNums.push_back(max(nums[2 * i], nums[2 * i + 1]));
+            }
+        }
+        return solve(n/2, newNums);
+    }
+    int minMaxGame(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int ans = solve(nums.size(), nums);
+        return ans;
+    }
+};
