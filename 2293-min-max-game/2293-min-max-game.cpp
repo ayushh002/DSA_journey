3aafdde9ff7f7b53f62eb03b3e8951ff2ead508c
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-11-19 09:50:03 +0530

    Time: 0 ms (100%), Space: 13.7 MB (13.18%) - LeetHub

--- 2293-min-max-game/2293-min-max-game.cpp
+++ 2293-min-max-game/2293-min-max-game.cpp
@@ -4,10 +4,12 @@ public:
         if(n==1) return nums[0];
         vector<int> newNums;
         for(int i = 0; i<n/2; i++){
-            if(i%2==0)
+            if(i%2==0){
                 newNums.push_back(min(nums[2 * i], nums[2 * i + 1]));
-            else
+            }
+            else{
                 newNums.push_back(max(nums[2 * i], nums[2 * i + 1]));
+            }
         }
         return solve(n/2, newNums);
     }
