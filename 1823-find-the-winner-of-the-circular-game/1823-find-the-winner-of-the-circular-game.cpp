060c11b439fa265769a56a95a0d76cd3d0f146f5
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-11-20 18:54:16 +0530

    Time: 0 ms (100%), Space: 7.9 MB (64.06%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,30 +1,12 @@
 class Solution {
 public:
-    int winner(vector<bool>& eliminated, int n, int index, int person_count, int k){
-        if(person_count==1){
-            for(int i = 0; i<n; i++){
-                if(!eliminated[i])
-                    return i+1;
-            }
-        }
+    int winner(int n, int k){
+        if(n==1) return 0;
 
-        int kill = (k-1) % person_count;
-        while(kill--){
-            index = (index+1) % n;
-            while(eliminated[index]) 
-                index = (index+1) % n;
-        }
-
-        eliminated[index] = 1;
-
-        while(eliminated[index]) 
-            index = (index+1) % n;
-
-        return winner(eliminated, n, index, person_count-1, k);
+        return (winner(n-1, k)+k) % n;
     }
     
     int findTheWinner(int n, int k) {
-        vector<bool> eliminated(n, 0);
-        return winner(eliminated, n, 0, n, k);
+        return winner(n, k)+1;
     }
 };
