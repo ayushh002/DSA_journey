79294ff5b402cbf0d0ad12058e0482d16e464242
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-12-01 12:28:18 +0530

    Time: 0 ms (100%), Space: 7.8 MB (86.48%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2) return n;
+        int a = 1, b = 2, c;
+        while(n>2){
+            c = a+b;
+            a = b;
+            b = c;
+            n--;
+        }
+        return c;
+    }
+};
