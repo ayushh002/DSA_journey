f9880689fd5d727e1969e9ed20fa6d940fcee00a
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-12-21 11:05:07 +0530

    Time: 0 ms (100%), Space: 10.9 MB (19.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int n, bool mark, vector<vector<int>>& dp){
+    // int solve(vector<int>& nums, int n, bool mark, vector<int>& dp){
+    //     if(n==0) return 0;
+    //     if(!mark && n==1) return nums[0];
+    //     else if(n==1) return 0;
+
+    //     if(dp[n] != -1) return dp[n];
+    //     if(n==nums.size()){
+    //         return dp[n] = max(nums[n-1]+solve(nums, n-2, !mark, dp), solve(nums, n-1, mark, dp));
+    //     }
+    //     return dp[n] = max(nums[n-1]+solve(nums, n-2, mark, dp), solve(nums, n-1, mark, dp));
+    // }
+
+    int solve(vector<int>& nums, int n, vector<int>& dp){
         if(n==0) return 0;
-        if(!mark && n==1) return nums[0];
-        else if(n==1) return 0;
+        if(n==1) return nums[0];
 
-        if (dp[n][mark] != -1) return dp[n][mark];
+        if(dp[n] != -1) return dp[n];
 
-        if(n==nums.size()){
-            return dp[n][mark] = max(nums[n-1]+solve(nums, n-2, !mark, dp), solve(nums, n-1, mark, dp));
-        }
-        return dp[n][mark] = max(nums[n-1]+solve(nums, n-2, mark, dp), solve(nums, n-1, mark, dp));
+        return dp[n] = max(nums[n-1]+solve(nums, n-2, dp), solve(nums, n-1, dp));
     }
-
 
     int rob(vector<int>& nums) {
         if(nums.size()==1)
             return nums[0];
         
-        vector<vector<int>> dp(nums.size()+1, vector<int>(2, -1));
+        vector<int> arr1(nums.begin(), nums.end() - 1);
+        vector<int> arr2(nums.begin() + 1, nums.end());
 
-        return solve(nums, nums.size(), 0, dp);
+        vector<int> dp1(arr1.size()+1, -1);
+        vector<int> dp2(arr2.size()+1, -1);
+        return max(solve(arr1, arr1.size(), dp1),solve(arr2,arr2.size(),dp2));
     }
 };
