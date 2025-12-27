80137bf79ee6bfe9ade380ced39604f774be4c19
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-12-27 13:18:10 +0530

    Time: 11 ms (97.7%), Space: 9 MB (96.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,15 +4,19 @@ public:
         // Bottom Up - tabulation
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        vector<int> dp(m+1, 0);
         for(int i = 1; i<=n; i++){
+            int prevDiag = 0;
             for(int j = 1; j<=m; j++){
+                int aboveElement = dp[j];
                 if(text1[i-1]==text2[j-1])
-                    dp[i][j] = 1+dp[i-1][j-1];
+                    dp[j] = 1+prevDiag;
                 else
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                    dp[j] = max(aboveElement, dp[j-1]);
+
+                prevDiag = aboveElement;
             }
         }
-        return dp[n][m];
+        return dp[m];
     }
 };
