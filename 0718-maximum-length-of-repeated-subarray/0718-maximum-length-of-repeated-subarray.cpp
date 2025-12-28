e171ae1eef824691c34b2e97414c7d567222df57
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-12-28 16:17:31 +0530

    Time: 42 ms (93.6%), Space: 14.7 MB (91.21%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
+        // tabulation space optimized
         int n = nums1.size();
         int m = nums2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-        for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                if(nums1[i-1]==nums2[j-1])
-                    dp[i][j] = 1+dp[i-1][j-1];
-            }
-        }
-        
+        vector<int> dp(m+1, 0);
+
         int maxLen = 0;
         for(int i = 1; i<=n; i++){
-            for(int j = 1; j<=m; j++){
-                maxLen = max(maxLen, dp[i][j]);
+            for(int j = m; j>=1; j--){ 
+                if(nums1[i-1]==nums2[j-1]){
+                    dp[j] = dp[j-1]+1;
+                    maxLen = max(maxLen, dp[j]);
+                }
+                else 
+                    dp[j] = 0;   // RESET dp[j]
             }
         }
+
         return maxLen;
     }
 };
