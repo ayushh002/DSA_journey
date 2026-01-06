f7931b5e517ed15d97c456c4df99ba21700002ff
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2026-01-06 14:45:01 +0530

    Time: 565 ms (12.68%), Space: 296.6 MB (5.85%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int solve(int index, int prev, vector<int>& nums, int n, vector<vector<int>>& dp){
-        if(index == n) return 0;
+        if(index==n)
+            return 0;
 
-        if (dp[index][prev + 1] != -1)
-        return dp[index][prev + 1];
+        if(dp[index][prev+1] != -1) return dp[index][prev+1];
 
-        int notTake = solve(index + 1, prev, nums, n, dp);
+        int notTake = solve(index+1, prev, nums, n, dp);
 
         int take = 0;
-        if(prev == -1 || nums[index] > nums[prev])
-            take = 1+solve(index + 1, index, nums, n, dp);
-        
-        return dp[index][prev + 1] = max(take, notTake);
+        if(prev==-1 || nums[index]>nums[prev])
+            take = 1+solve(index+1, index, nums, n, dp);
+
+        return dp[index][prev+1] = max(take, notTake);
     }
     int lengthOfLIS(vector<int>& nums) {
         // recursive - Gives TLE
