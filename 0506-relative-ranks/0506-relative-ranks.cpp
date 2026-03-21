6db83d5051c3a489006e6bbafd17eacc8acc59d2
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2026-03-21 17:42:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
-        unordered_map<int, string> rank = {
-            {1, "Gold Medal"},
-            {2, "Silver Medal"},
-            {3, "Bronze Medal"}
-        };
-        vector<string> ans(n);
-
-        unordered_map<int, int> idxMap;
-        for(int i = 0; i<n; i++)
-            idxMap[score[i]] = i;
         
-        priority_queue<int> pq;
-        for(int x : score) pq.push(x);
-        int count = 1;
-        while(!pq.empty()){
-            int top = pq.top();
-            int index = idxMap[top];
+        // 1. Store (score, index)
+        vector<pair<int, int>> vec;
+        for(int i = 0; i<n; i++)
+            vec.push_back({score[i], i});
+        
+        // Step 2: sort in descending order of score
+        sort(vec.begin(), vec.end(), greater<pair<int, int>>());
+        
+        // 3. Prepare answer (assign ranks)
+        vector<string> ans(n);
+        for(int i = 0; i<n; i++){
+            int idx = vec[i].second;
 
-            if(count<=3)
-                ans[index] = rank[count];
-            else
-                ans[index] = to_string(count);
-            
-            count++;
-            pq.pop();
+            if(i==0) ans[idx] = "Gold Medal";
+            else if(i==1) ans[idx] = "Silver Medal";
+            else if(i==2) ans[idx] = "Bronze Medal";
+            else ans[idx] = to_string(i+1);
         }
         return ans;
     }
