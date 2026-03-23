9e33187555324fc83968a819b1a42d886c00f5bd
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2026-03-23 19:52:46 +0530

    Time: 47 ms (12.02%), Space: 61.7 MB (20.78%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,52 +1,13 @@
 class Solution {
 public:
     // O(n log k) - Optimized
-    vector<int> minHeap;
-    void insert(int num, int k){
-
-        int index = minHeap.size();
-        minHeap.push_back(num);
-
-        while(index>0 && minHeap[(index-1)/2] > minHeap[index]){
-            swap(minHeap[(index-1)/2], minHeap[index]);
-            index = (index-1)/2;
-        }
-
-        if(minHeap.size()>k){
-            deleteTop(k);
-        }
-    }
-
-    void deleteTop(int k){
-        int size = minHeap.size();
-
-        minHeap[0] = minHeap[size-1];
-        minHeap.pop_back();
-
-        if(minHeap.size()==0) return;
-        Heapify(0);
-    }
-
-    void Heapify(int index){
-        int size = minHeap.size();
-        int smallest = index;
-        int left = 2*index+1;
-        int right = 2*index+2;
-        if(left<size && minHeap[left]<minHeap[smallest])
-        smallest = left;
-        if(right<size && minHeap[right]<minHeap[smallest])
-        smallest = right;
-
-        if(smallest!=index){
-            swap(minHeap[index], minHeap[smallest]);
-            Heapify(smallest);
-        }
-    }
-
     int findKthLargest(vector<int>& nums, int k) {
-        for(int i = 0; i<nums.size(); i++){
-            insert(nums[i], k);
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int num: nums){
+            pq.push(num);
+            if(pq.size()>k)
+                pq.pop();
         }
-        return minHeap[0];
+        return pq.top();
     }
 };
