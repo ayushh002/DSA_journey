6cbdc8a5976815c7a4426a051b8253f3bf53666c
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2026-03-24 20:19:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int s1 = num1.length(), s2 = num2.length();
-        int max = s1 > s2 ? s1 : s2;
+        int n1 = num1.length(), n2 = num2.length();
 
-        string ans = "";
         int carry = 0;
-        while(max--){
+        string ans = "";
+        while(n1 || n2){
             int digit1 = 0, digit2 = 0;
-            if(s1)
-                digit1 = (num1[--s1] - '0');
-            if(s2)
-                digit2 = (num2[--s2] - '0');
-
-            int sum = digit1+digit2+carry;
-            carry = sum/10;
+            if(n1)
+                digit1 = num1[--n1] - '0';
+            if(n2)
+                digit2 = num2[--n2] - '0';
+            
+            int sum = carry + digit1 + digit2;
+            carry = sum / 10;
             sum %= 10;
             ans += (sum + '0');
         }
+
         if(carry)
             ans += '1';
+        
         reverse(ans.begin(), ans.end());
         return ans;
     }
